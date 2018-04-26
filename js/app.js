8dc28525efda08ecdab0693e777e77cda134078b
JessicaMP <jessica95.mp@gmail.com>
2018-04-26 13:28:02 -0500

    Agregando seccion about y help

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
 $(".slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     dots: true,
     infinite: false,
     speed: 500,
   });
+
+$(".slider-customers").slick({
+  // normal options...
+  infinite: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
